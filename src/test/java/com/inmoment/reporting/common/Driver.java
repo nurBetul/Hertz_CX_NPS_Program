2381d05e9f0c299c497688fd3e75facf187f98b9
nyaz <nyaz@inmoment.com>
2020-02-21 15:46:42 -0500

    First commit

---
+++ src/test/java/com/inmoment/reporting/common/Driver.java
@@ -0,0 +1,47 @@
+package com.inmoment.reporting.common;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class Driver {
+    public static WebDriver driver;
+    public static WebDriver getDriver(){
+        if (driver == null){
+            setUpDriver();
+        }
+        return driver;
+    }
+
+    public static void setUpDriver(){
+        if(driver == null){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+    }
+
+    public static void closeDriver(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    public static String getProp (String input){
+        try{
+            Properties prop = new Properties();
+            FileInputStream fis = new FileInputStream("src/test/java/com/inmoment/reporting/common/global.properties");
+            prop.load(fis);
+            return prop.getProperty(input);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+}
